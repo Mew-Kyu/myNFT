65bafd556f189a24f5cd16323688c440728ccd67
Long Nguyen <nhlong143@gmail.com>
2023-05-24 16:53:19 +0700

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
 import styled from "styled-components";
 import search from "assets/search.svg";
+import noti from "assets/noti.svg";
+import avatar from "assets/avatar.svg";
 
 const HeaderStyled = styled.div`
   position: fixed;
   z-index: 99;
-  width: 100%;
+  width: 80%;
   box-sizing: border-box;
   height: 64px;
   padding-left: 32px;
@@ -14,7 +16,8 @@ const HeaderStyled = styled.div`
   background-color: #caeae6;
 `;
 const SearchForm = styled.div`
-  width: 30%;
+  width: 50%;
+  padding: 0 26px;
   height: 39px;
   border-radius: 35px;
   background: #ffffff;
@@ -25,7 +28,41 @@ const SearchForm = styled.div`
     height: 24px;
   }
   input {
+    width: 100%;
     border: none;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 21px;
+    outline: none;
+    text-indent: 15px;
+  }
+  input::placeholder {
+    color: #b9b8bc;
+  }
+`;
+const RightHeader = styled.div`
+  height: 100%;
+  display: flex;
+  align-items: center;
+  padding-right: 32px;
+  div {
+    width: 56px;
+    height: 56px;
+    background: #fff;
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-right: 24px;
+  }
+  div img {
+    width: 24px;
+    height: 24px;
+  }
+  img {
+    width: 56px;
+    height: 56px;
+    border-radius: 50%;
   }
 `;
 
@@ -34,12 +71,17 @@ const Header = () => {
     <HeaderStyled>
       <SearchForm>
         <img src={search} alt="" />
-        <input type="text" />
+        <input
+          type="text"
+          placeholder="Search Item, Collection and Account.."
+        />
       </SearchForm>
-      <div>
-        <span>noti</span>
-        <span>avatar</span>
-      </div>
+      <RightHeader>
+        <div>
+          <img src={noti} alt="Notification icon" />
+        </div>
+        <img src={avatar} alt="Avatar" />
+      </RightHeader>
     </HeaderStyled>
   );
 };
